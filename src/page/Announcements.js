0350baa824d00269b689a3dd9b9a0a5f30c20077
xmcp <xmcp@pku.edu.cn>
2023-07-17 00:25:37 +0800

    add paging for announcement

--- src/page/Announcements.js
+++ src/page/Announcements.js
@@ -1,4 +1,5 @@
-import {Empty, Skeleton, Card, Tag} from 'antd';
+import {useState} from 'react';
+import {Empty, Skeleton, Card, Tag, Pagination} from 'antd';
 import {NotificationOutlined} from '@ant-design/icons';
 
 import {Reloader} from './GameLoading';
@@ -8,6 +9,8 @@ import {TimestampAgo} from '../utils';
 
 import './Announcements.less';
 
+const PAGE_SIZE = 10;
+
 export function Announcement({announcement, extra}) {
     return (
         <div className="announcement">
@@ -16,7 +19,7 @@ export function Announcement({announcement, extra}) {
                 title={<>
                     <Tag style={{fontSize: '1em', fontWeight: 'bold'}} color="blue">
                     <NotificationOutlined /> {announcement.title}</Tag>
-                    {' '}发布于 <TimestampAgo ts={announcement.timestamp_s} />
+                    {' '}<TimestampAgo ts={announcement.timestamp_s} />发布
                 </>}
             >
                 <TemplateStr name="announcement">{announcement.content}</TemplateStr>
@@ -27,20 +30,26 @@ export function Announcement({announcement, extra}) {
 
 export function Announcements() {
     let [error, data, load_data] = useWishData('announcements');
+    let [page, set_page] = useState(1);
 
     if(error)
         return <Reloader message={error.error_msg} reload={load_data} />;
     if(data===null)
         return <Skeleton />;
+    if(data.list.length===0)
+        return (
+            <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="暂无公告" />
+        );
+
+    let cur_data = data.list.slice((page-1)*PAGE_SIZE, page*PAGE_SIZE);
 
     return (
         <div className="announcement-list">
-            {data.list.map((ann)=>(
+            <Pagination current={page} onChange={(x)=>set_page(x)} total={data.list.length} pageSize={PAGE_SIZE} />
+            {cur_data.map((ann)=>(
                 <Announcement key={ann.id} announcement={ann} />
             ))}
-            {data.list.length===0 &&
-                <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="暂无公告" />
-            }
+            <Pagination current={page} onChange={(x)=>set_page(x)} total={data.list.length} pageSize={PAGE_SIZE} />
         </div>
     );
 }
