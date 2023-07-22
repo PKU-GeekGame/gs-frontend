eaec4f38a2dec416daff86a99c4f339fdb820e72
xmcp <xmcp@pku.edu.cn>
2023-07-22 22:41:17 +0800

    modernize antd timeline

--- src/page/Triggers.js
+++ src/page/Triggers.js
@@ -36,20 +36,21 @@ export function Triggers() {
     return (
         <div>
             <br />
-            <Timeline mode="left">
-                {data.list.map((trigger)=>(
-                    <Timeline.Item
-                        key={trigger.timestamp_s}
-                        label={<WithStatus status={trigger.status}>
+            <Timeline
+                mode="left"
+                items={data.list.map((trigger)=>({
+                    color: {'pst': 'gray', 'prs': 'blue', 'ftr': 'black'}[trigger.status],
+                    dot: {'pst': null, 'prs': <ClockCircleOutlined />, 'ftr': null}[trigger.status],
+                    label: (
+                        <WithStatus status={trigger.status}>
                             {trigger.timestamp_s===0 ? '--' : format_ts(trigger.timestamp_s)}
-                        </WithStatus>}
-                        color={{'pst': 'gray', 'prs': 'blue', 'ftr': 'black'}[trigger.status]}
-                        dot={{'pst': null, 'prs': <ClockCircleOutlined />, 'ftr': null}[trigger.status]}
-                    >
+                        </WithStatus>
+                    ),
+                    children: (
                         <WithStatus status={trigger.status}><Splitter>{trigger.name}</Splitter></WithStatus>
-                    </Timeline.Item>
-                ))}
-            </Timeline>
+                    ),
+                }))}
+            />
             {data.list.length===0 &&
                 <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="暂无赛程" />
             }
