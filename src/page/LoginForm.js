c69c4651c1631363e3ec1112f74e94e54b1052e2
xmcp <xmcp@pku.edu.cn>
2024-03-21 21:59:30 +0800

    fix login form style

--- src/page/LoginForm.js
+++ src/page/LoginForm.js
@@ -10,7 +10,10 @@ export function LoginForm() {
 
             <div className="login-instruction">
                 <p><b>报名须知</b></p>
-                <pre style={{textAlign: 'left'}}>TODO: 报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。</pre>
+                <div style={{textAlign: 'left', whiteSpace: 'pre-wrap'}}>
+                    TODO: 报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。
+                    报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。
+                </div>
                 <p>
                     <Button type="primary" size="large" onClick={()=>window.open('https://example.com/TODO')}>
                         <RightCircleOutlined /> 报名比赛
@@ -27,7 +30,7 @@ export function LoginForm() {
                         <Input name="token" addonBefore="密码" style={{width: '350px'}} placeholder="报名通过后将通过邮件发送给您" size="large" />
                     </p>
                     <p>
-                        <Button size="large" htmlType="submit">
+                        <Button size="large" block htmlType="submit">
                             <RightCircleOutlined/> 登录
                         </Button>
                     </p>
