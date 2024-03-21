65ac610376042b822efa260d33fa84256a363060
xmcp <xmcp@pku.edu.cn>
2024-03-21 22:03:03 +0800

    make block buttons

--- src/page/Game.js
+++ src/page/Game.js
@@ -455,7 +455,7 @@ export function Game() {
             <div className="slim-container">
                 <TemplateFile name="game" />
                 <br />
-                <Button type="primary" size="large" onClick={()=>window.location.href='#/login/form'}><LoginOutlined /> 报名参赛</Button>
+                <Button type="primary" size="large" block onClick={()=>window.location.href='#/login/form'}><LoginOutlined /> 报名参赛</Button>
             </div>
         );
     else
--- src/page/LoginForm.js
+++ src/page/LoginForm.js
@@ -1,6 +1,8 @@
 import {Button, Input} from 'antd';
 import {RightCircleOutlined} from '@ant-design/icons';
 
+import {TemplateFile} from '../widget/Template';
+
 import './LoginForm.less';
 
 export function LoginForm() {
@@ -10,15 +12,11 @@ export function LoginForm() {
 
             <div className="login-instruction">
                 <p><b>报名须知</b></p>
-                <div style={{textAlign: 'left', whiteSpace: 'pre-wrap'}}>
-                    TODO: 报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。
-                    报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。报名须知。
-                </div>
-                <p>
-                    <Button type="primary" size="large" onClick={()=>window.open('https://example.com/TODO')}>
-                        <RightCircleOutlined /> 报名比赛
-                    </Button>
-                </p>
+                <TemplateFile name="registration" />
+                <br />
+                <Button type="primary" size="large" block onClick={()=>window.open('https://example.com/TODO')}>
+                    <RightCircleOutlined /> 报名参赛
+                </Button>
 
                 <br />
                 <hr />
@@ -27,13 +25,11 @@ export function LoginForm() {
                 <p><b>登录</b></p>
                 <form action="/service/auth/token" method="get">
                     <p>
-                        <Input name="token" addonBefore="密码" style={{width: '350px'}} placeholder="报名通过后将通过邮件发送给您" size="large" />
+                        <Input name="token" addonBefore="密码" placeholder="报名通过后将通过邮件发送给您" size="large" />
                     </p>
-                    <p>
-                        <Button size="large" block htmlType="submit">
-                            <RightCircleOutlined/> 登录
-                        </Button>
-                    </p>
+                    <Button size="large" block htmlType="submit">
+                        <RightCircleOutlined/> 登录
+                    </Button>
                 </form>
             </div>
         </div>
