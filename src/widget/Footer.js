4d8fdfecb57c799f77e4198b6892d3f8a8a74441
xmcp <xmcp@pku.edu.cn>
2024-03-24 16:43:49 +0800

    remove link to gs

--- src/widget/Footer.js
+++ src/widget/Footer.js
@@ -1,12 +1,10 @@
 import './Footer.less';
-import {ExtLink} from '../utils';
 
 export function Footer() {
     return (
         <div className="footer">
             <p>
-                Powered by Project <b><ExtLink href="https://github.com/PKU-GeekGame/guiding-star">Guiding Star</ExtLink></b>
-                {process.env.REACT_APP_BUILD_INFO ? ' ('+process.env.REACT_APP_BUILD_INFO+')' : null}
+                build {process.env.REACT_APP_BUILD_INFO ? process.env.REACT_APP_BUILD_INFO : '---'}
             </p>
         </div>
     );
