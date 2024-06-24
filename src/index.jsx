ad1382150b0a2df1b11ac4083f3f50c0a29a6eec
xmcp <xmcp@pku.edu.cn>
2024-06-24 17:30:57 +0800

    use react18 strict mode

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 //import ReactDOMServer from 'react-dom/server';
 import {ConfigProvider, Alert, App as AntdApp} from 'antd';
 import {HashRouter} from 'react-router-dom';
+import { StyleProvider } from '@ant-design/cssinjs';
 
 import {GameInfoProvider} from './logic/GameInfo';
 import {App} from './App';
@@ -12,33 +13,39 @@ import zhCN from 'antd/es/locale/zh_CN';
 import './index.less';
 
 let component = (
-    <ConfigProvider
-        autoInsertSpaceInButton={false}
-        locale={zhCN}
-        theme={{
-            token: {
-                colorPrimary: '#096dd9',
-                colorLink: '#096dd9',
-                colorText: '#000',
-                fontWeightStrong: 500,
-            },
-            components: {
-                App: {
-                    fontSize: 16,
+    <React.StrictMode>
+        <ConfigProvider
+            button={{
+                autoInsertSpace: false,
+            }}
+            locale={zhCN}
+            theme={{
+                token: {
+                    colorPrimary: '#096dd9',
+                    colorLink: '#096dd9',
+                    colorText: '#000',
+                    fontWeightStrong: 500,
                 },
-            }
-        }}
-    >
-        <HashRouter future={{v7_startTransition: true}}>
-            <Alert.ErrorBoundary>
-                <GameInfoProvider>
-                    <AntdApp>
-                        <App />
-                    </AntdApp>
-                </GameInfoProvider>
-            </Alert.ErrorBoundary>
-        </HashRouter>
-    </ConfigProvider>
+                components: {
+                    App: {
+                        fontSize: 16,
+                    },
+                }
+            }}
+        >
+            <StyleProvider hashPriority="high">
+                <HashRouter future={{v7_startTransition: true}}>
+                    <Alert.ErrorBoundary>
+                        <GameInfoProvider>
+                            <AntdApp>
+                                <App />
+                            </AntdApp>
+                        </GameInfoProvider>
+                    </Alert.ErrorBoundary>
+                </HashRouter>
+            </StyleProvider>
+        </ConfigProvider>
+    </React.StrictMode>
 );
 
 //console.log(ReactDOMServer.renderToString(component));
