6ec4aae6b8e3c5acd5adf47e6e7900d2c9769e6f
xmcp <xmcp@pku.edu.cn>
2024-07-01 17:53:26 +0800

    add noderef for transition

--- src/widget/Transition.jsx
+++ src/widget/Transition.jsx
@@ -1,17 +1,22 @@
+import {useRef} from 'react';
 import {SwitchTransition, CSSTransition} from 'react-transition-group';
 
 import './Transition.less';
 
 export function Transition({cur, children}) {
+    let noderef = useRef();
     return (
         <SwitchTransition>
             <CSSTransition
                 key={cur}
                 classNames="app-transition"
                 timeout={83}
+                nodeRef={noderef}
                 unmountOnExit
             >
-                {children}
+                <div ref={noderef}>
+                    {children}
+                </div>
             </CSSTransition>
         </SwitchTransition>
     );
