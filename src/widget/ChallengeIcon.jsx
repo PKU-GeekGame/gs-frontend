2a77672dba6ac4f4d87b39ebc36e0f8f86f8d4f0
xmcp <xmcp@pku.edu.cn>
2024-07-01 21:29:39 +0800

    add asterisk in status if deducted

--- src/widget/ChallengeIcon.jsx
+++ src/widget/ChallengeIcon.jsx
@@ -3,24 +3,42 @@ import {Tag} from 'antd';
 
 import './ChallengeIcon.less';
 
-export function ChallengeIcon({status}) {
-    if(status==='untouched')
-        return <BorderOutlined className="challenge-icon-untouched" />;
-    else if(status==='partial')
-        return <MinusSquareTwoTone className="challenge-icon-partial" twoToneColor="#1e63bd" />;
-    else if(status==='passed')
-        return <CheckSquareTwoTone className="challenge-icon-passed" twoToneColor="#1ab500" />;
+function parse_status(status) {
+    let s = status.split('-');
+    if(s.length===2)
+        return [s[0], s[1]==='deducted'];
     else
-        return '??';
+        return [status, false];
+}
+
+export function ChallengeIcon({status}) {
+    let [passed, deduct] = parse_status(status);
+
+    let icon;
+    if(passed==='untouched')
+        icon = <BorderOutlined className="challenge-icon-untouched" />;
+    else if(passed==='partial')
+        icon = <MinusSquareTwoTone className="challenge-icon-partial" twoToneColor="#1e63bd" />;
+    else if(passed==='passed')
+        icon = <CheckSquareTwoTone className="challenge-icon-passed" twoToneColor="#1ab500" />;
+    else
+        icon = '??';
+
+    return deduct ? <span className="status-icon-deducted">{icon}</span> : icon;
 }
 
 export function FlagIcon({status}) {
-    if(status==='untouched')
-        return <FlagTwoTone className="flag-icon-passed" twoToneColor="#a7a7a7" />;
-    else if(status==='passed')
-        return <FlagTwoTone className="flag-icon-passed" twoToneColor="#1ab500" />;
+    let [passed, deduct] = parse_status(status);
+
+    let icon;
+    if(passed==='untouched')
+        icon = <FlagTwoTone className="flag-icon-passed" twoToneColor="#a7a7a7" />;
+    else if(passed==='passed')
+        icon = <FlagTwoTone className="flag-icon-passed" twoToneColor="#1ab500" />;
     else
-        return '??';
+        icon = '??';
+
+    return deduct ? <span className="status-icon-deducted">{icon}</span> : icon;
 }
 
 export function CategoryBadge({color, children}) {
