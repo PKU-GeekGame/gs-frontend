a27b361215986f186e851d00119d919bad58bb26
xmcp <xmcp@pku.edu.cn>
2024-07-03 20:26:03 +0800

    use compression during build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import {compression} from 'vite-plugin-compression2'
+import zlib from 'zlib';
 
 //const API_URL = 'http://localhost:8010';
 const API_URL = 'https://geekgame.pku.edu.cn';
@@ -10,6 +12,7 @@ export default defineConfig(() => {
             outDir: 'build',
             sourcemap: process.env.GENERATE_SOURCEMAP!=='false',
             chunkSizeWarningLimit: 1500,
+            reportCompressedSize: false,
             rollupOptions: {
                 output: {
                     compact: true,
@@ -18,12 +21,24 @@ export default defineConfig(() => {
                         vendor: ['antd', '@ant-design/icons', 'react', 'react-dom', 'react-router-dom', 'react-transition-group', 'react-lazyload', 'react-timeago'],
                     },
                 }
-            }
+            },
+            minify: 'terser',
         },
         esbuild: {
             legalComments: 'none',
         },
-        plugins: [react()],
+        plugins: [
+            react(),
+            compression({
+                algorithm: 'brotliCompress',
+                compressionOptions: {
+                    params: {
+                        [zlib.constants.BROTLI_PARAM_MODE]: zlib.constants.BROTLI_MODE_TEXT,
+                        [zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY,
+                    },
+                },
+            }),
+        ],
         server: {
             open: true,
             port: 3000,
