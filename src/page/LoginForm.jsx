b23a12318bcd6414a7f5e177ca02bb322a5c6483
xmcp <xiaoyuanan2000@sina.com>
2025-10-13 20:17:57 +0800

    change auth to password-based

--- src/page/LoginForm.jsx
+++ src/page/LoginForm.jsx
@@ -13,28 +13,18 @@ export function LoginForm() {
             <br />
 
             <div className="login-instruction">
-                <p><b>- 报名须知 -</b></p>
+                <p><b>- 登录 -</b></p>
                 <div style={{textAlign: 'left'}}>
                     <TemplateFile name="registration" />
                 </div>
                 <br />
 
-                <div>
-                    <Button type="primary" size="large" block onClick={()=>{window.location.href=LOGIN_URL;}}>
-                        <RightCircleOutlined /> 报名参赛
-                    </Button>
-                </div>
-
-                <br />
-                <br />
-                <br />
-
-                <p><b>- 登录 -</b></p>
-                <form action="/service/auth/token" method="get">
+                <form action="/service/auth/password" method="post">
                     <p>
-                        <Input name="token" addonBefore="密码" required placeholder="报名通过后将通过邮件发送给您" size="large" />
+                        <Input name="email" addonBefore="注册邮箱" required placeholder="" size="large" />
+                        <Input name="password" addonBefore="密码" required placeholder="" size="large" />
                     </p>
-                    <Button size="large" block htmlType="submit">
+                    <Button type="primary" size="large" block htmlType="submit">
                         <RightCircleOutlined/> 进入比赛
                     </Button>
                 </form>
